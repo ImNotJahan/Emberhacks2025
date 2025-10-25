5ef3369fc373e8b454545eaef2cf148f6f22e3f0
Endernasha <ender6doc@mail.ru>
2025-10-25 17:06:18 -0400

    parce_json created in main

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, request, jsonify
+from latex_extension.data import MeasuredData
+
 app = Flask(__name__)
 
 @app.route("/")
--- main.py
+++ main.py
@@ -1,8 +1,18 @@
+import json
+from latex_extension.data import MeasuredData
+
 def main():
     """
     Initialize server and everything
     """
     pass
+
+def parce_json(json_text):
+    data = json.loads(json_text)["variables"]
+    newdata = {}
+    for dic in data:
+        newdata[dic["name"]] = MeasuredData(dic["value"],dic["uncertainty"])
+    return newdata
 
 if __name__ == "__main__":
     main()
