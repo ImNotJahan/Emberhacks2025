62cf594868963f8ca199729be262444c593c0b35
Endernasha <ender6doc@mail.ru>
2025-10-25 15:36:46 -0400

    CheckBoxDone

--- app.py
+++ app.py
@@ -9,7 +9,20 @@ def home():
 def send():
     data = request.get_json()
     text = data.get("text", "")
+    isFull = data.get("isFull", "")
     print(text)
+    print(isFull)
     return jsonify({"message": f"Answer: {text}"})
+
+"""
+@app.route("/submit", methods=["POST"])
+def submit():
+    isFull = request.form.get("fullWork") 
+    if isFull:
+        print("Full")
+    else:
+        print("NotFull")
+    return None
+"""
 
 #python -m flask run
--- static/script.js
+++ static/script.js
@@ -1,16 +1,21 @@
 document.getElementById("Btn_input").addEventListener("click", async () => {
   const input = document.getElementById("input").value;
-
-  // send to Flask backend via POST
+  const fw = document.getElementById("full_work");
+  var checked = false;
+  if (fw.checked){
+    checked =true;
+  }
+  else{
+    checked = false;
+  }
   const response = await fetch("/send", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ text: input })
+    body: JSON.stringify({ text: input , isFull: checked})
   });
-
-  // get JSON reply
-  const data = await response.json();
-
-  // show result
+  box.addEventListener("change", async () => {
+    const payload = { checked: fw.checked };
+  });
   document.getElementById("responseText").textContent = data.message;
+  const box = document.getElementById("darkMode");
 });
