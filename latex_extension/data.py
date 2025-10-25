cd67b022d2300c17cdd70f30390ad734aacbebf5
Jahan Rashidi <hiimjahan@gmail.com>
2025-10-25 19:19:23 -0400

    Add LaTeX templates for all MeasuredData operations

--- latex_extension/data.py
+++ latex_extension/data.py
@@ -21,75 +21,166 @@ class MeasuredData(md):
     # for the most part, just overriding methods on the physics_tools MeasuredData to add more functionality
 
     def __add__(self, other):
-        value_step = r"@x@+@y@"
-        uncertainty_step = r"\sqrt{@s_x@)^2+@s_y@^2}"
-        step_variables = [self, other]
-
-        result = super().__add__(other)
-        return MeasuredData(result.value, result.error(), value_step, uncertainty_step, step_variables)
+        return MeasuredData(
+            (result := super().__add__(other)).value,
+            result.error(),
+            r"@x@+@y@",
+            r"\sqrt{@s_x@^2+@s_y@^2}" if isinstance(other, MeasuredData) else r"@s_x@",
+            [self, other]
+        )
 
     def __radd__(self, other):
-        return super().__radd__(other)
+        return MeasuredData(
+            (result := super().__radd__(other)).value,
+            result.error(),
+            r"@y@+@x@",
+            r"\sqrt{@s_y@^2+@s_x@^2}" if isinstance(other, MeasuredData) else r"@s_y@",
+            [self, other]
+        )
 
     def __sub__(self, other):
-        return super().__sub__(other)
+        return MeasuredData(
+            (result := super().__sub__(other)).value,
+            result.error(),
+            r"@x@-@y@",
+            r"\sqrt{@s_x@^2+@s_y@^2}"  if isinstance(other, MeasuredData) else r"@s_x@",
+            [self, other]
+        )
 
     def __mul__(self, other):
-        return super().__mul__(other)
+        return MeasuredData(
+            (result := super().__mul__(other)).value,
+            result.error(),
+            r"@x@\cdot@y@",
+            r"@x@\cdot@y@\sqrt{\frac{@s_x@}{@x@}^2+\frac{@s_y@}{@y@}^2}" if isinstance(other, MeasuredData) else
+            r"@s_x@\cdot@y@",
+            [self, other]
+        )
 
     def __rmul__(self, other):
-        return super().__rmul__(other)
+        return MeasuredData(
+            (result := super().__rmul__(other)).value,
+            result.error(),
+            r"@y@\cdot@x@",
+            r"@x@\cdot@y@\sqrt{\frac{@s_y@}{@y@}^2+\frac{@s_x@}{@x@}^2}" if isinstance(other, MeasuredData) else
+            r"@s_x@\cdot@y@",
+            [self, other]
+        )
 
     def __truediv__(self, other):
-        return super().__truediv__(other)
+        return MeasuredData(
+            (result := super().__truediv__(other)).value,
+            result.error(),
+            r"\frac{@x@}{@y@}",
+            r"@x@\cdot@y@\sqrt{\frac{@s_x@}{@x@}^2+\frac{@s_y@}{@y@}^2}" if isinstance(other, MeasuredData) else
+            r"\frac{@s_x@}{@y@}",
+            [self, other]
+        )
 
     def __pow__(self, other: int):
-        return super().__pow__(other)
+        return MeasuredData(
+            (result := super().__pow__(other)).value, # abs(other * self.value ** (other - 1) * s)
+            result.error(),
+            r"@x@^{@y@}",
+            r"\left|@y@\cdot@s_x@\cdot@x@^{@y@ - 1}\right|",
+            [self, other]
+        )
 
     def sine(self):
-        return super().sine()
+        return MeasuredData(
+            (result := super().sine()).value,  # abs(s * math.cos(self.value))
+            result.error(),
+            r"\sin @x@",
+            r"\left|@s_x@ \cos @x@\right|",
+            [self]
+        )
 
     def cosine(self):
-        return super().cosine()
+        return MeasuredData(
+            (result := super().cosine()).value,
+            result.error(),
+            r"\cos @x@",
+            r"\left|@s_x@ \sin @x@\right|",
+            [self]
+        )
 
     def tangent(self):
-        return super().tangent()
+        return MeasuredData(
+            (result := super().tangent()).value,
+            result.error(),
+            r"\tan @x@",
+            r"\left|@s_x@ \sec @x@^2\right|",
+            [self]
+        )
 
     def arctan(self):
-        return super().arctan()
+        # s / (1 + self.value ** 2)
+        return MeasuredData(
+            (result := super().arctan()).value,
+            result.error(),
+            r"\arctan @x@",
+            r"\frac{@s_x@}{1+@x@^2}",
+            [self]
+        )
 
     def arcsin(self):
-        return super().arcsin()
+        # s / math.sqrt(1 - self.value ** 2)
+        return MeasuredData(
+            (result := super().arcsin()).value,
+            result.error(),
+            r"\arcsin @x@",
+            r"\frac{@s_x@}{\sqrt{1-@x@^2}}",
+            [self]
+        )
 
     def __neg__(self):
-        return super().__neg__()
+        return MeasuredData(
+            (result := super().__neg__()).value,
+            result.error(),
+            r"-@x@",
+            r"@s_x@",
+            [self]
+        )
 
     def __abs__(self):
-        return super().__abs__()
+        return MeasuredData(
+            (result := super().__abs__()).value,
+            result.error(),
+            r"\left|@x@\right|",
+            r"@s_x@",
+            [self]
+        )
 
     # from here on out, we have some latex_extension unique methods for MeasuredData
 
     def uncertainty_equations(self, max_depth: int) -> list[tuple[str, tuple]]:
         pass
 
-    def recent_step(self, value_step, plug_in_vars=True, trunc_nums=True) -> str:
+    def recent_step(self, value_step, plug_in_vars=True, trunc_nums=True, add_parenthesis=True) -> str:
         if value_step:
             step = self.value_step
         else:
             step = self.uncertainty_step
 
         if plug_in_vars:
+            md_p = lambda x: isinstance(x, MeasuredData) # measured data predicate (i.e., is measured data)
+            md_s = lambda x: str(x).split("±") # measured data split
+            en_v = lambda: enumerate(self.step_variables) # enumerate (step) variables
+
             if trunc_nums:
-                norm_vars = {var_letters[i]: str(v).split("±")[0] for i, v in enumerate(self.step_variables)}
-                err_vars  = {"s_" + var_letters[i]: str(v).split("±")[1] for i, v in enumerate(self.step_variables)}
+                norm_vars = {var_letters[i]: md_s(v)[0] for i, v in en_v() if md_p(v)}
+                norm_vars.update({var_letters[i]: v for i, v in en_v() if not md_p(v)})
+                err_vars  = {"s_" + var_letters[i]: md_s(v)[1] for i, v in en_v() if md_p(v)}
             else:
-                norm_vars = {var_letters[i]: v.value for i, v in enumerate(self.step_variables)}
-                err_vars = {"s_" + var_letters[i]: v.error() for i, v in enumerate(self.step_variables)}
+                norm_vars = {var_letters[i]: v.value for i, v in en_v() if md_p(v)}
+                norm_vars.update({var_letters[i]: v for i, v in en_v() if not md_p(v)})
+                err_vars = {"s_" + var_letters[i]: v.error() for i, v in en_v() if md_p(v)}
+
             vals = norm_vars
             vals.update(err_vars)
         else:
             vals = self_mapping_var_letters
-        return at_format(step, vals, True)
+        return at_format(step, vals, add_parenthesis)
 
     def all_steps_sequential(self, plug_in_vars=True, trunc_nums=True) -> tuple[list[str], list[str]]:
         value_steps       = []
@@ -97,8 +188,8 @@ class MeasuredData(md):
 
         # recursively add steps of any previously calculated MeasuredDatas to step lists
         def apnd_step(dp: MeasuredData):
-            value_steps.append(dp.recent_step(True, plug_in_vars, trunc_nums))
-            uncertainty_steps.append(dp.recent_step(False, plug_in_vars, trunc_nums))
+            value_steps.append(dp.recent_step(True, plug_in_vars, trunc_nums, False))
+            uncertainty_steps.append(dp.recent_step(False, plug_in_vars, trunc_nums, False))
 
             for sdp in dp.step_variables:
                 if isinstance(sdp, MeasuredData) and sdp.has_steps:
