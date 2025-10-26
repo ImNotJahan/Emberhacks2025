fb81802fc840fc6b597533f8ac89948797b72116
Jahan Rashidi <hiimjahan@gmail.com>
2025-10-25 20:00:34 -0400

    Move parse_json out of main

---
+++ llm_to_server_glue.py
@@ -0,0 +1,23 @@
+import json
+from latex_extension.data import MeasuredData
+
+def parse_json(json_text: str) -> tuple[dict[str, MeasuredData | float], str]:
+    """
+    This function takes in the JSON response of the LLM, containing the equation and variables provided by the
+    user, and then parses them into a state more usable for computation.
+    """
+    parsed_json = json.loads(json_text)
+
+    json_dict = parsed_json["variables"]
+    equation = parsed_json["equation"]["python_string_repr"]
+
+    parsed_dict = {}
+    for variable in json_dict:
+        # if the uncertainty is zero, we just treat it as a float
+        # otherwise, turn it into a MeasuredData object
+        if variable["uncertainty"] == 0:
+            parsed_dict[variable["name"]] = float(variable["value"])
+        else:
+            parsed_dict[variable["name"]] = MeasuredData(variable["value"], variable["uncertainty"])
+
+    return parsed_dict, equation
--- main.py
+++ main.py
@@ -1,20 +1,12 @@
 import json
 from latex_extension.data import MeasuredData
+from app import start_server
 
 def main():
     """
     Initialize server and everything
     """
-    pass
-
-def parce_json(json_text):
-    data = json.loads(json_text)["variables"]
-    equation = json.loads(json_text)["equation"]["python_string_repr"]
-    newdata = {}
-    for dic in data:
-        newdata[dic["name"]] = MeasuredData(dic["value"],dic["uncertainty"])
-    
-    return newdata, equation
+    start_server()
 
 if __name__ == "__main__":
     main()
