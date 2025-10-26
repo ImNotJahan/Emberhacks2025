659f234d9eda37daecb8c08d074811f9c943ec79
Jahan Rashidi <hiimjahan@gmail.com>
2025-10-26 09:23:26 -0400

    Remove user escapade

--- resources/db_init_requests.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS requests (
-    id          INTEGER PRIMARY KEY,
-    author_id   INTEGER NOT NULL,
-    req         VARCHAR NOT NULL,
-    response    VARCHAR NOT NULL,
-    created     TEXT NOT NULL,
-    CONSTRAINT fk_request_to_user FOREIGN KEY (author_id) REFERENCES users (id)
-)
--- resources/db_init_users.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE IF NOT EXISTS users (
-    id        INTEGER PRIMARY KEY,
-    username  VARCHAR(64) NOT NULL,
-    email     VARCHAR(128) NOT NULL,
-    password  VARCHAR(128) NOT NULL
-);
