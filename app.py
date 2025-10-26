6aba0ad6908093f5013f4cdc96eb41b63a1ea5c8
Endernasha <ender6doc@mail.ru>
2025-10-26 00:04:54 -0400

    draft
    f

--- app.py
+++ app.py
@@ -4,11 +4,27 @@ from server_to_latex_glue import send_llm_parsing
 from llm_to_server_glue import parse_json
 from flask import Response
 import json
+from flask_httpauth import HTTPBasicAuth
+from werkzeug.security import check_password_hash, generate_password_hash
+
+auth = HTTPBasicAuth()
+USERS = {
+    "alice": generate_password_hash("1111"),
+    "bob":   generate_password_hash("1111"),
+}
+@auth.verify_password
+def verifylogin(username, password):
+    data = request.get_json()
+    username = data.get("username", "")
+    password = data.get("password", "")
+    if username in USERS and check_password_hash(USERS[username], password):
+        return username  # return any user object/identifier
+    else:
+        return render_template("accesdenined.html")
 
 app = Flask(__name__)
 manager = RequestsManager()
 console_txt = "Console"
-
 @app.route("/")
 def home(): #Sets the website to index - main html
     return render_template("index.html")
@@ -18,37 +34,27 @@ def sighnin():
 @app.route("/login")
 def login():
     return render_template("login.html")
-
 @app.route("/getInput", methods=["POST"])  #Gets the req
 def getInput():
     data = request.get_json()
     text = data.get("text", "")
-
     response = manager.get_response(text)
-
     # check if an error occurred
     if response[:5] == "[ERR]":
         return Response(
             json.dumps({"failed": True, "console": response[5:]}),
             mimetype="application.json"
         )
-
     answerDic, equationPy = parse_json(response)
     value, solution, equation = send_llm_parsing(equationPy,answerDic)
-
     console_txt = "Done!"
-
     payload = {"val": value, "sol": solution, "equ": equation, "console": console_txt, "failed": False}
-
     return Response(
         json.dumps(payload, ensure_ascii=False),
         mimetype="application/json"
     )
-
 def start_server():
     print("=== Starting server ===")
-
     app.run()
-
     print("=== Server closed ===")
 #python -m flask run
--- static/script.js
+++ static/script.js
@@ -24,3 +24,17 @@ document.getElementById("Btn_input").addEventListener("click", async () => {
   document.getElementById("console").innerHTML = data.console;//Prints out the values
   console.log(data)
 });
+document.getElementById("Btn_login").addEventListener("click", async () => {
+  const user = document.getElementById("user_l").value;
+  const password = document.getElementById("password_l").value;
+  const response = await fetch("/verifylogin", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ username: user, password:  password})
+  });
+  const data = await response.json();
+  
+});
+document.getElementById("Btn_sighn").addEventListener("click", async () => {
+  
+});
